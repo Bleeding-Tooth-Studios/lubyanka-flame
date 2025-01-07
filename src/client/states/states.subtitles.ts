80fd1244993b0cf82b0fc503091efe2e43304817
sergei <adrian.illumina@gmail.com>
2025-01-07 19:16:55 +0700

    Added subtitle push function

--- src/client/states/states.subtitles.ts
+++ src/client/states/states.subtitles.ts
@@ -1,4 +1,7 @@
 import { atom } from "@rbxts/charm";
 import { SubtitleData } from "shared/types/types.subtitle";
 
+/**
+ * A queue (array) of all the subtitles pending. 1 = top, 5 = bottom. Use unshift on the array to insert at the top.
+ */
 export const subtitlesQueue = atom<SubtitleData[]>([]);
