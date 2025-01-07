0b4061a7b3d84d31e0fbe6df1102948c2bd55205
sergei <adrian.illumina@gmail.com>
2025-01-07 19:59:38 +0700

    modified:   client/ui/uicomponents/Timer.tsx

--- src/client/ui/uicomponents/Timer.tsx
+++ src/client/ui/uicomponents/Timer.tsx
@@ -1,47 +1,43 @@
-import {InstanceProps, ReactNode} from "@rbxts/react";
+import { InstanceProps, ReactNode } from "@rbxts/react";
 import React from "@rbxts/react";
 import { ANCHORS, POSITIONSCALES } from "../uiconsts/uiconsts.util";
 import { TEXT_STYLES } from "../uiconsts/uiconsts.textstyles";
 import { useTimer } from "@rbxts/pretty-react-hooks";
 import { toBinding } from "@rbxts/pretty-react-hooks";
 
-
 //formatSeconds(2,40) --> count down from 00:38
 //formatSeconds(0,70) --> count down from 01:10
 function formatSeconds(elapsedSeconds: number, totalSeconds: number): string {
-    // Calculate remaining time
-    const remainingTime = totalSeconds - elapsedSeconds;
-    if (remainingTime < 0 ){
-        return ""
-    }
+	// Calculate remaining time
+	const remainingTime = totalSeconds - elapsedSeconds;
+	if (remainingTime < 0) {
+		return "";
+	}
 
-    // Calculate minutes and seconds
-    const minutes = math.floor(remainingTime / 60);
-    const seconds = remainingTime % 60;
+	// Calculate minutes and seconds
+	const minutes = math.floor(remainingTime / 60);
+	const seconds = remainingTime % 60;
 
-    // Format minutes and seconds with leading zeros
-    const formattedMinutes = minutes < 10 ? "0" + minutes : tostring(minutes) 
-    const formattedSeconds = seconds < 10 ? "0" + seconds : tostring(seconds) 
+	// Format minutes and seconds with leading zeros
+	const formattedMinutes = minutes < 10 ? "0" + minutes : tostring(minutes);
+	const formattedSeconds = seconds < 10 ? "0" + seconds : tostring(seconds);
 
-    return (formattedMinutes+":"+formattedSeconds);
+	return formattedMinutes + ":" + formattedSeconds;
 }
 
-export function TimerText(props:{
-    totalSeconds: number;
-}): ReactNode {
-
-    const timer = useTimer()
-    const second = toBinding(timer.value.map((value) => math.floor(value)))
-    timer.start
-    return(
-        <textlabel
-            key="TimerText"
-            {...TEXT_STYLES.MEDIUM}
-            Text={timer.value.map((value) => formatSeconds((math.floor(value)),5))}
-            BackgroundColor3={Color3.fromRGB(255, 0, 0)}
+export function TimerText(props: { totalSeconds: number }): ReactNode {
+	const timer = useTimer();
+	const second = toBinding(timer.value.map((value) => math.floor(value)));
+	timer.start;
+	return (
+		<textlabel
+			key="TimerText"
+			{...TEXT_STYLES.MEDIUM}
+			Text={timer.value.map((value) => formatSeconds(math.floor(value), 5))}
+			BackgroundColor3={Color3.fromRGB(255, 0, 0)}
 			TextColor3={Color3.fromRGB(255, 255, 255)}
-            AnchorPoint={ANCHORS.CENTER_TOP}
-            Position={POSITIONSCALES.CENTER_TOP}
-        />
-    )
+			AnchorPoint={ANCHORS.CENTER_TOP}
+			Position={POSITIONSCALES.CENTER_TOP}
+		/>
+	);
 }
